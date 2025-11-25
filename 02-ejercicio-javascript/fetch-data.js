192caa56d70001ea8e7e4d20474650f1d4bb7653
stalindlcruz <stalindlcruz@gmail.com>
2025-11-25 14:11:16 -0500

    fetch data done

--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -1,1 +1,26 @@
 /* Aquí va la lógica para mostrar los resultados de búsqueda */
+
+const container = document.querySelector(".jobs-listings");
+
+fetch("./data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jobs) => {
+    jobs.forEach((job) => {
+      const list = document.createElement("li");
+      const article = document.createElement("article");
+      article.className = "job-listing-card";
+
+      article.innerHTML = `
+      <div>
+        <h3>${job.titulo}</h3>
+        <small>${job.empresa} | ${job.ubicacion}</small>
+        <p>${job.descripcion}</p>
+      </div>
+      <button class="button-apply-job">Aplicar</button>
+      `;
+      list.appendChild(article);
+      container.appendChild(list);
+    });
+  });
