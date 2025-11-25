bf62420614fa8db6148b99c3e4a04475afdb6d7f
stalindlcruz <stalindlcruz@gmail.com>
2025-11-25 14:37:01 -0500

    boton aplicar donde

--- 02-ejercicio-javascript/apply-button.js
+++ 02-ejercicio-javascript/apply-button.js
@@ -1,1 +1,13 @@
 /* Aquí va la lógica para dar funcionalidad al botón de "Aplicar" */
+
+const jobsListings = document.querySelector(".jobs-listings");
+
+jobsListings?.addEventListener("click", (event) => {
+  const element = event.target;
+
+  if (element.matches("button")) {
+    element.textContent = "Aplicado!";
+    element.classList.add("is-applied");
+    element.disabled = true;
+  }
+});
