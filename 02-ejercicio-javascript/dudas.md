f23ca6f5d21e205c9d31c578fa8b435948752f1c
stalindlcruz <stalindlcruz@gmail.com>
2025-12-20 13:03:32 -0500

    document fragment understood

--- 02-ejercicio-javascript/dudas.md
+++ 02-ejercicio-javascript/dudas.md
@@ -1,1 +1,7 @@
 <!-- Aquí puedes introducir tus dudas sobre el ejercicio, la consigna, la corrección, etc -->
+
+Hola, muchas gracias sobre los comentarios y las mejoras proporcionadas en los comentarios.
+
+# fetch-data.js
+
+Primero, no conocía lo de createDocumentFragment(). Lo investigué y me gustó mucho saber que cuando lo usamos es como una caja vacía (virtual) para crear dentro nuestros elementos y luego transferirlos al elemento que tenemos en el DOM. Lo más importante es que mejora el rendimiento porque solo causa un reflow en lugar de múltiples, ya que construimos todo en memoria primero y lo insertamos de una sola vez.
--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -12,7 +12,7 @@ fetch("./data.json")
     Para que sirve esto? Para evitar re dibujar el HTML cada vez que insertamos un elemento dentro del forEach. Lo que hacemos es: agregamos los elementos en el contenedor virtual, y una vez estén todos, pintamos de una sola vez lo que hay en el contenedor sobre el DOM.
     Esto mejora bastante el rendimiento, sobre todo cuando tenemos muchos elementos :)
     */
-    const documentFragment = document.createDocumentFragment()
+    const documentFragment = document.createDocumentFragment();
 
     jobs.forEach((job) => {
       const list = document.createElement("li");
@@ -33,8 +33,8 @@ fetch("./data.json")
       <button class="button-apply-job">Aplicar</button>
       `;
       list.appendChild(article);
-      documentFragment.appendChild(list)
+      documentFragment.appendChild(list);
     });
-    
+
     container.appendChild(documentFragment);
   });
