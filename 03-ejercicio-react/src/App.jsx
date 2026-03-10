074f3c03646e076794e4cdfb52ea816d361dee8f
stalindlcruz <stalindlcruz@gmail.com>
2026-03-10 15:29:18 -0400

    improving route Component

--- 03-ejercicio-react/src/App.jsx
+++ 03-ejercicio-react/src/App.jsx
@@ -3,17 +3,11 @@ import { Route } from "./components/Route.jsx";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 
-import { SearchPage } from "./pages/Search";
-import { HomePage } from "./pages/Home";
-import { NotFoundPage } from "./pages/404";
-
 function App() {
   return (
     <>
       <Header />
-      <Route path="/" component={HomePage} />
-      <Route path="/search" component={SearchPage} />
-      <Route path="/404" component={NotFoundPage} />
+      <Route />
       <Footer />
     </>
   );
--- 03-ejercicio-react/src/components/Route.jsx
+++ 03-ejercicio-react/src/components/Route.jsx
@@ -1,11 +1,21 @@
 import { useRouter } from "../hooks/useRouter.jsx";
+import { HomePage } from "../pages/Home";
+import { SearchPage } from "../pages/Search";
+import { NotFoundPage } from "../pages/404.jsx";
 
-export function Route({ path, component: Component }) {
+export function Route() {
   const { currentPathname } = useRouter();
 
-  if (currentPathname !== path) {
-    return null;
+  const routes = [
+    { path: "/", component: HomePage },
+    { path: "/search", component: SearchPage },
+  ];
+
+  const route = routes.find((route) => route.path === currentPathname);
+
+  if (route) {
+    return <route.component />;
   }
 
-  return <Component />;
+  return <NotFoundPage />;
 }
