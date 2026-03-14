8c3ccafc7e6c0f7d98303c9c9ff22484ed428bff
stalindlcruz <stalindlcruz@gmail.com>
2026-03-14 16:52:40 -0400

    extras done

--- 03-ejercicio-react/src/components/JobListings.jsx
+++ 03-ejercicio-react/src/components/JobListings.jsx
@@ -2,9 +2,19 @@ import { JobCard } from "./JobCard.jsx";
 
 export function JobListings({ jobs }) {
   return (
-    <div className="jobs-listings">
+    <div style={{ marginBlock: "2rem" }} className="jobs-listings">
       {jobs.length === 0 && (
-        <p>No se han encontrado empleos que coincidan con la búsqueda</p>
+        <p
+          style={{
+            color: "#fff",
+            textAlign: "center",
+            marginBlock: "2rem",
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+          }}
+        >
+          No se han encontrado empleos que coincidan con la búsqueda 😔
+        </p>
       )}
 
       {jobs.map((job) => (
