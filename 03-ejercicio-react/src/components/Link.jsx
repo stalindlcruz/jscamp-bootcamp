aa642e5fb7f3ec158c49d4163fde61341bd74900
stalindlcruz <stalindlcruz@gmail.com>
2026-03-15 14:08:22 -0400

    active link done

--- 03-ejercicio-react/src/components/Link.jsx
+++ 03-ejercicio-react/src/components/Link.jsx
@@ -1,15 +1,37 @@
 import { useRouter } from "../hooks/useRouter.jsx";
+import styles from "./Link.module.css";
 
-export function Link({ href, children, ...restOfProps }) {
-  const { navigateTo } = useRouter();
+export function Link({
+  href,
+  children,
+  exact = true,
+  className = "",
+  activeClassName = styles.active,
+  ...restOfProps
+}) {
+  const { navigateTo, currentPathname } = useRouter();
 
   const handleClick = (event) => {
     event.preventDefault();
     navigateTo(href);
   };
 
+  const isActive = exact
+    ? currentPathname === href
+    : currentPath.startsWith(href);
+
+  const finalClassName = isActive
+    ? `${className} ${activeClassName}`.trim()
+    : className.trim();
+
   return (
-    <a href={href} {...restOfProps} onClick={handleClick}>
+    <a
+      className={finalClassName}
+      href={href}
+      aria-current={isActive ? "page" : undefined}
+      {...restOfProps}
+      onClick={handleClick}
+    >
       {children}
     </a>
   );
