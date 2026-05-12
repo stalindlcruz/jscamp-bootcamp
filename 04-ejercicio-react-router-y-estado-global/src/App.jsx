7c46bce4dfcad219770016cb1f38f2fc83c0b462
madeval <alvarez.fing@gmail.com>
2026-05-12 18:15:52 -0300

    Add comment acknowledging correct ProtectedRoute implementation in profile route

--- 04-ejercicio-react-router-y-estado-global/src/App.jsx
+++ 04-ejercicio-react-router-y-estado-global/src/App.jsx
@@ -35,6 +35,7 @@ function App() {
           <Route
             path="/profile"
             element={
+              /* Muy bien! */
               <ProtectedRoute>
                 <ProfilePage />
               </ProtectedRoute>
