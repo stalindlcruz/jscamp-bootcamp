a41ce0016834076ce70072d7fcf3ed48c3ed76c8
stalindlcruz <stalindlcruz@gmail.com>
2026-07-14 12:51:19 -0400

    Fifth exercise: Cors Middleware, done

--- 07-ejercicio-api-rest-con-express-y-mvc/app.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { jobsRouter } from "./routes/jobs.js";
 
+import { middlewareCors } from "./middlewares/cors.js";
 import { DEFAULTS } from "./config.js";
 
 const PORT = process.env.PORT || DEFAULTS.PORT;
 const app = express();
 
+app.use(middlewareCors());
 app.use(express.json());
 
 app.use("/jobs", jobsRouter);
@@ -13,3 +15,5 @@ app.use("/jobs", jobsRouter);
 app.listen(PORT, () => {
   console.log(`Servidor levantado en http://localhost:${PORT}`);
 });
+
+export default app;
--- 07-ejercicio-api-rest-con-express-y-mvc/middlewares/cors.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/middlewares/cors.js
@@ -1,3 +1,24 @@
-import cors from 'cors'
+import cors from "cors";
 
 /* Aquí debe ir la lógica de tu middleware */
+const ACCEPTED_ORIGINS = [
+  "http://localhost:3000",
+  "http://localhost:1234",
+  "https://midu.dev",
+  "http://jscamp.dev",
+  "http://localhost:5173",
+];
+
+export function middlewareCors({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) {
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (acceptedOrigins.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Origen no permitido"));
+    },
+  };
+
+  return cors(corsOptions);
+}
