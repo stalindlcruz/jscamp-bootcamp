9c9a9e87ecdf1b80a51ed1a70aecb0a167a0dd57
stalindlcruz <stalindlcruz@gmail.com>
2026-08-09 12:04:05 -0400

    Primitive and literal types done.

--- 10-ejercicio-typescript/types.ts
+++ 10-ejercicio-typescript/types.ts
@@ -6,3 +6,32 @@ export type WorkMode =
 export type ApplicationStatus = 
 export type Technology = 
 */
+
+export type ExperienceLevel = "junior" | "mid" | "senior" | "lead";
+
+export type WorkMode = "remoto" | "presencial" | "hibrido";
+
+export type ApplicationStatusStatus =
+  | "pending"
+  | "reviewing"
+  | "accepted"
+  | "rejected";
+
+export type Technology =
+  | "react"
+  | "node"
+  | "python"
+  | "java"
+  | "javascript"
+  | "typescript"
+  | "flutter"
+  | "android"
+  | "ios"
+  | "swift"
+  | "kotlin"
+  | "dart"
+  | "go"
+  | "rust"
+  | "php"
+  | "ruby"
+  | "c#";
