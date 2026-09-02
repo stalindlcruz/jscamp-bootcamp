c26c46f90121c9e613eb5d31cba3d6a39d5633f8
stalindlcruz <stalindlcruz@gmail.com>
2026-09-02 17:26:26 -0400

    Task 6: Volumes and bind mounts

--- 13-ejercicio-docker/06-volumenes/server.js
+++ 13-ejercicio-docker/06-volumenes/server.js
@@ -1,11 +1,11 @@
-const { createServer } = require('node:http');
-const fs = require('node:fs');
-const path = require('node:path');
+const { createServer } = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
 
 // Intentamos leer la URL de conexión a la Base de Datos
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_HOST = process.env.DB_HOST || "localhost";
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join(__dirname, 'data', 'visitas.txt');
+const DATA_FILE = path.join(__dirname, "data", "visitas.txt");
 
 // Asegurar que exista el directorio de datos para persistencia (volúmenes / bind mounts)
 if (!fs.existsSync(path.dirname(DATA_FILE))) {
@@ -14,31 +14,36 @@ if (!fs.existsSync(path.dirname(DATA_FILE))) {
 
 // Inicializar el contador si no existe
 if (!fs.existsSync(DATA_FILE)) {
-  fs.writeFileSync(DATA_FILE, '0', 'utf-8');
+  fs.writeFileSync(DATA_FILE, "0", "utf-8");
 }
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(200, { "Content-Type": "application/json" });
 
-  if (req.url === '/visitar') {
-    let visitas = parseInt(fs.readFileSync(DATA_FILE, 'utf-8') || '0', 10);
+  if (req.url === "/visitar") {
+    let visitas = parseInt(fs.readFileSync(DATA_FILE, "utf-8") || "0", 10);
     visitas++;
-    fs.writeFileSync(DATA_FILE, visitas.toString(), 'utf-8');
-    
-    return res.end(JSON.stringify({
-      message: '¡Vamos! 📈',
-      contador: visitas
-    }));
+    fs.writeFileSync(DATA_FILE, visitas.toString(), "utf-8");
+
+    return res.end(
+      JSON.stringify({
+        message: "¡Vamos! 📈",
+        contador: visitas,
+      }),
+    );
   }
 
   // Ruta por defecto
-  let visitasActuales = fs.readFileSync(DATA_FILE, 'utf-8') || '0';
-  res.end(JSON.stringify({
-    message: 'HOLA QUÉ PASA CHAVALES',
-    instrucciones: 'Visita /visitar para registrar una nueva visita persistente en disco.',
-    contador_actual: parseInt(visitasActuales, 10),
-    db_config: `Conectando por red al host de base de datos: "${DB_HOST}"`
-  }));
+  let visitasActuales = fs.readFileSync(DATA_FILE, "utf-8") || "0";
+  res.end(
+    JSON.stringify({
+      message: "Hola, este es el nuevo mensaje usando bind mounts.",
+      instrucciones:
+        "Visita /visitar para registrar una nueva visita persistente en disco.",
+      contador_actual: parseInt(visitasActuales, 10),
+      db_config: `Conectando por red al host de base de datos: "${DB_HOST}"`,
+    }),
+  );
 });
 
 server.listen(PORT, () => {
